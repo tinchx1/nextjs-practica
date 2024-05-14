5c3b5e56bdb77ba61b1ea3e8dc39bab6c8bb2fc5
tinchx1 <tinchomarchi34@gmail.com>
2024-05-14 12:01:07 -0300

    course nextjs finished

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
 export default function PageCustomers() {
   return <h1>Hello world!</h1>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,15 @@
 import "@/app/ui/global.css";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Acme Dashboard',
+    template: '%s | Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
